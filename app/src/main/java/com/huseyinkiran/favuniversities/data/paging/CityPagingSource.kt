2c72578f93950483a0be66e7de11bb72b08d087d
Hüseyin <hsyn.krn76@gmail.com>
2025-07-19 18:30:07 +0300

    Update CityPagingSource

--- app/src/main/java/com/huseyinkiran/favuniversities/data/paging/CityPagingSource.kt
+++ app/src/main/java/com/huseyinkiran/favuniversities/data/paging/CityPagingSource.kt
@@ -1,37 +1,46 @@
 package com.huseyinkiran.favuniversities.data.paging
 
+import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.huseyinkiran.favuniversities.domain.model.CityUIModel
 import com.huseyinkiran.favuniversities.domain.repository.UniversityRepository
+import okio.IOException
+import retrofit2.HttpException
 
 class CityPagingSource(
     private val repository: UniversityRepository
-): PagingSource<Int, CityUIModel>() {
+) : PagingSource<Int, CityUIModel>() {
 
     override fun getRefreshKey(state: PagingState<Int, CityUIModel>): Int? {
-       return state.anchorPosition?.let {  anchorPosition ->
-           val anchorPage = state.closestPageToPosition(anchorPosition)
-           anchorPage?.nextKey?.minus(1) ?: anchorPage?.prevKey?.plus(1)
-       }
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CityUIModel> {
-        val page = params.key ?: 1
+        val pageIndex = params.key ?: PAGE_INDEX
 
         return try {
-            val cities = repository.getUniversities(page)
-            val totalPage = repository.getTotalPage()
-            val isLastPage = page >= totalPage
+            val cities = repository.getUniversities(pageIndex)
 
             LoadResult.Page(
                 data = cities,
-                prevKey = if (page == 1) null else page - 1,
-                nextKey = if (isLastPage) null else page + 1
+                prevKey = if (pageIndex == PAGE_INDEX) null else pageIndex - 1,
+                nextKey = if (cities.isEmpty()) null else pageIndex + 1
             )
 
-        } catch (e: Exception) {
-            LoadResult.Error(e)
+        } catch (exception: IOException) {
+            LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            LoadResult.Error(exception)
         }
+    }
+
+    companion object {
+        const val NETWORK_PAGE_SIZE = 30
+        const val PAGE_INDEX = 1
+
     }
 }
